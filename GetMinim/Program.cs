bcd93eaa61c3a92d65b698ff1332696ec6435c6c
Alexandru Boghean <alexandrubgn98@gmail.com>
2023-06-20 20:30:08 +0300

    methods assigment

---
+++ GetMinim/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Your first number is: ");
+        int firstNum = int.Parse(Console.ReadLine());
+
+        Console.Write("Your second number is: ");
+        int secondNum = int.Parse(Console.ReadLine());
+
+        GetMin(firstNum, secondNum);
+    }
+
+    public static void GetMin(int num1, int num2)
+    {
+        Console.WriteLine($"The smallest number between {num1} and {num2} is: {Math.Min(num1, num2)}.");
+    }
+}
